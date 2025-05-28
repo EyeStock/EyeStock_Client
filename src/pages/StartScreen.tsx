a3598541f5d4ea22381d3c97f593c58c55495d2b
psm1st <psm1st@sookmyung.ac.kr>
2025-05-28 23:25:54 +0900

    feat : login 로직 screen에 적용

--- src/pages/StartScreen.tsx
+++ src/pages/StartScreen.tsx
@@ -1,33 +1,55 @@
-import React from "react";
-import { useNavigation } from "@react-navigation/native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Image } from "react-native";
-import styled from "styled-components/native";
-import { RootStackParamList } from "../navigation/RootStackParamList";
+import React, {useEffect} from 'react';
+import {Alert, Image} from 'react-native';
+import styled from 'styled-components/native';
+import {useNavigation} from '@react-navigation/native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {RootStackParamList} from '../navigation/RootStackParamList';
+import {useBiometricAuth} from '../hooks/useBiometricAuth';
 
-type Navigation = NativeStackNavigationProp<RootStackParamList, "StartScreen">;
+type Navigation = NativeStackNavigationProp<RootStackParamList, 'StartScreen'>;
 
 export default function StartScreen() {
   const navigation = useNavigation<Navigation>();
+  const {login} = useBiometricAuth();
+
+  useEffect(() => {
+    const doBiometricLogin = async () => {
+      try {
+        await login();
+        navigation.navigate('TalkStartScreen');
+      } catch (err: any) {
+        Alert.alert(
+          '지문 인증 실패',
+          err?.message || '지문 인증에 실패했습니다.',
+        );
+      }
+    };
+
+    doBiometricLogin();
+  }, []);
 
   return (
-    <FullTouchable onPress={() => navigation.navigate("TalkStartScreen")}>
+    <FullTouchable activeOpacity={1}>
       <Container>
         <Inner>
-          <StyledImage source={require('../assets/logo.png')} resizeMode="contain" />
+          <StyledImage
+            source={require('../assets/logo.png')}
+            resizeMode="contain"
+          />
           <Title>EYESTOCK</Title>
         </Inner>
       </Container>
     </FullTouchable>
   );
 }
+
 const FullTouchable = styled.TouchableOpacity`
   flex: 1;
 `;
 
 const Container = styled.View`
   flex: 1;
-  background-color: ${({ theme }) => theme.color.white};
+  background-color: ${({theme}) => theme.color.white};
   padding: 40px 24px;
   justify-content: center;
 `;
@@ -43,9 +65,9 @@ const StyledImage = styled.Image`
 `;
 
 const Title = styled.Text`
-  color: ${({ theme }) => theme.color.black};
+  color: ${({theme}) => theme.color.black};
   font-size: 24px;
-  font-family: ${({ theme }) => theme.font.regular};
+  font-family: ${({theme}) => theme.font.regular};
   font-weight: bold;
   margin-bottom: 24px;
 `;
