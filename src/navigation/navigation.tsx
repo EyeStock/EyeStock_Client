541a5c5e185d1a59082b5f7532a4263ba474cb48
psm1st <psm1st@sookmyung.ac.kr>
2025-06-04 13:39:45 +0900

    feat : navigation에 speechToTextScreen

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,23 +1,25 @@
-import React from "react";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
-
-export type RootStackParamList = {
-    StartScreen: undefined;
-    TalkStartScreen: undefined;
-  };
-
-import StartScreen from "../pages/StartScreen";
-import TalkStartScreen from "../pages/TalkStartScreen";
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import StartScreen from '../pages/StartScreen';
+import TalkStartScreen from '../pages/TalkStartScreen';
+import SpeechToTextScreen from '../pages/SpeechToTextScreen';
+import {RootStackParamList} from './RootStackParamList';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="StartScreen" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        initialRouteName="StartScreen"
+        screenOptions={{headerShown: false}}>
         <Stack.Screen name="StartScreen" component={StartScreen} />
         <Stack.Screen name="TalkStartScreen" component={TalkStartScreen} />
+        <Stack.Screen
+          name="SpeechToTextScreen"
+          component={SpeechToTextScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
